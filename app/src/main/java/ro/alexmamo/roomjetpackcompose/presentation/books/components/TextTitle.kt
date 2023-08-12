c532bd58ba2d11607d1d6ad0add87f4483f0e632
Julian Sukrisna <smallest87@gmail.com>
2023-08-12 17:03:02 +0700

    Update Koding

--- app/src/main/java/ro/alexmamo/roomjetpackcompose/presentation/books/components/TextTitle.kt
+++ app/src/main/java/ro/alexmamo/roomjetpackcompose/presentation/books/components/TextTitle.kt
@@ -3,6 +3,7 @@ package ro.alexmamo.roomjetpackcompose.presentation.books.components
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -12,6 +13,7 @@ fun TextTitle(
     Text(
         text = bookTitle,
         color = Color.DarkGray,
-        fontSize = 25.sp
+        fontSize = 25.sp,
+        fontWeight = FontWeight.Bold
     )
 }
